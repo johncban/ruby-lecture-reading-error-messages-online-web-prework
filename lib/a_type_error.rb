8dd2a9a977b2adc0a651b2bfada15f9e4aca030b
johncban <johncban@gmail.com>
2019-04-07 20:20:41 +0000

    Automatically backed up by Learn

--- lib/a_type_error.rb
+++ lib/a_type_error.rb
@@ -2,4 +2,4 @@ num = 1 + 1
 
 (1 + num.to_i).to_s + "is not the loneliest number"
 
-# Source: https://www.ruby-forum.com/t/type-casting-and-connversion-in-ruby/227272/3
+# Source: https://stackoverflow.com/questions/25397484/string-cant-be-coerced-into-fixnum-typeerror
